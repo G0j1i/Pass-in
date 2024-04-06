0b7ea4b645bb137d6e75cdfec4b3c3adce74d4c2
Goji <gabriel16moch@gmail.com>
2024-04-06 02:23:11 -0300

    feat: Implementing connection with db

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -1,5 +1,6 @@
 from src.models.settings.base import Base
 from sqlalchemy import Column, String, Integer
+
 class Events(Base):
     __tablename__ = "events"
 
@@ -9,4 +10,10 @@ class Events(Base):
     slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
 
-
+class Attendees(Base): 
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    event_id = Column(String, nullable=False)
+    created_at = Column(nullable=False, default=True, )
+    attendees_event_id_fkey = Column('id', Foareignkey=())
